e5ae61abbc46e4babe297a3b552beb4f16a3f9cb
Aef_SB <46419278+aeff60@users.noreply.github.com>
2022-03-10 04:09:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return "Hello BorntoDev2!!"  
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
